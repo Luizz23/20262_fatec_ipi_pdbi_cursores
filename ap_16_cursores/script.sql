3c21ea2bc400f70b8af7e7b9f087fd3196156278
Luiz Fabiano <fabianop1910@gmail.com>
2026-09-22 11:15:52 -0300

    learn(ap16_cursores): encerra bloquinho 2.6.1

--- ap_16_cursores/script.sql
+++ ap_16_cursores/script.sql
@@ -1,34 +1,66 @@
--- PROCESSANDO LINHA POR LINHA 
-DO $$
+--Cursor não vincluado de query dinamica
+--exibindo nomes de youtubers que que comecaram a partir de um ano especifico
+
+DO$$
 
 DECLARE
+--1 Declaração 
+cur_nomes_a_partir_de REFCURSOR;
+v_youtuber VARCHAR(200)
+v_ano INT := 2008;
+v_nome_tabela_VARCHAR(200) := 'tb_top_youtubers':
 
--- Declaração do CURSOR
-cur_nomes_youtubers_REFCURSOR;
-v_youtuber VARCHAR(200),
-BEGIN
-
---2 Abertura do cursor
-OPEN cur_nomes_youtubers FOR
-SELECT youtuber
-FROM tb_top_youtubers;
+BEGIN 
+-- 2 ABERTURA
+OPEN cur_nomes_a_partir de FOR EXECUTE
+format(
+	'SELECT youtuber FROM %s WHERE started >= $1',
+	v_nome_tabela
+)
+USING v_ano;
 LOOP
-
--- 3 Recuperação dos dados de interesse
-FETCH cur_nomes_youtubers INTO v_youtuber;
+--3 Recuperação de dados
+FETCH cur_nomes_a_partir_de INTO v_youtuber;
 EXIT WHEN NOT FOUND;
 RAISE NOTICE '%', v_youtuber;
-END LOOP;
-
---4 Fechamento
-CLOSE cur_nomes_youtubers;
-END;
+EXIT LOOP
+-- 4 Fechamneto do cursor
+CLOSE cur_nomes_a_partir_de;
+END 
 $$
 
 
+-- PROCESSANDO LINHA POR LINHA 
+-- DO $$
 
-ALTER TABLE tb_top_youtubers
- ALTER COLUMN video_views TYPE BIGINT;
+-- DECLARE
+
+-- -- Declaração do CURSOR
+-- cur_nomes_youtubers_REFCURSOR;
+-- v_youtuber VARCHAR(200),
+-- BEGIN
+
+-- --2 Abertura do cursor
+-- OPEN cur_nomes_youtubers FOR
+-- SELECT youtuber
+-- FROM tb_top_youtubers;
+-- LOOP
+
+-- -- 3 Recuperação dos dados de interesse
+-- FETCH cur_nomes_youtubers INTO v_youtuber;
+-- EXIT WHEN NOT FOUND;
+-- RAISE NOTICE '%', v_youtuber;
+-- END LOOP;
+
+-- --4 Fechamento
+-- CLOSE cur_nomes_youtubers;
+-- END;
+-- $$
+
+
+
+-- ALTER TABLE tb_top_youtubers
+--  ALTER COLUMN video_views TYPE BIGINT;
 
 
 -- ([A-Z])[A-Z]([A-Z]-[0-9]{4})
